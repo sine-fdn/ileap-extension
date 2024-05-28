74d25b31a464142fe2d81f91adca9f273b844a50
Raimundo Henriques <raimundo.henriques@gmail.com>
2024-05-28 15:06:44 +0100

    feat: add iLEAP data model and generate schemas

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -1,80 +1,40 @@
-use schemars::{
-    gen::SchemaGenerator,
-    schema::{Schema, SchemaObject},
-    JsonSchema,
-};
+use ileap_extension::*;
+use regex::Regex;
+use schemars::schema_for;
+use serde_json::to_string_pretty;
+use std::fs::File;
+use std::io::{Error, Write};
 
-#[derive(JsonSchema)]
-/// # UN/LOCODE Data Model Extension (Version 0.1.0-20230522)
-///
-/// The LOCODE data model extension. See https://sine-fdn.github.io/locode-extension for details
-pub struct LoCodeExtension {
-    /// The property `unlocode`. See https://sine-fdn.github.io/locode-extension/#element-attrdef-productfootprint-unlocode
-    #[schemars(schema_with = "locode_schema")]
-    pub unlocode: String,
+fn main() -> Result<(), Error> {
+    generate_schema::<ShipmentFootprint>()?;
+    generate_schema::<Toc>()?;
+    generate_schema::<Tad>()?;
 
-    /// The property `coordinates`. See https://sine-fdn.github.io/locode-extension/#element-attrdef-productfootprint-coordinates
-    #[schemars(schema_with = "coordinates_schema")]
-    #[schemars(default)]
-    pub coordinates: Option<String>,
-
-    //pub coordinates: Option<Coordinates>,
-    /// The property `name`. See https://sine-fdn.github.io/locode-extension/#element-attrdef-productfootprint-name
-    pub name: Option<String>,
-
-    /// The property `remarks`. See https://sine-fdn.github.io/locode-extension/#element-attrdef-productfootprint-remarks
-    pub remarks: Option<String>,
+    Ok(())
 }
 
-fn locode_schema(gen: &mut SchemaGenerator) -> Schema {
-    let mut s: SchemaObject = Option::<String>::json_schema(gen).into();
+fn generate_schema<T: schemars::JsonSchema>() -> Result<(), Error> {
+    let type_name = std::any::type_name::<T>();
+    let type_name = type_name.split("::").collect::<Vec<&str>>();
+    let type_name = type_name.last().unwrap_or(&"Could not parse type name");
 
-    s.metadata = Some(Box::new(schemars::schema::Metadata {
-        description: Some("The UN/LOCODE is a geographic coding scheme developed and maintained by United Nations Economic Commission for Europe (UNECE).".into()),
-        examples: vec![serde_json::Value::String("ZMCHZ".into())],
-        ..Default::default()
-    }));
-    s.string = Some(Box::new(schemars::schema::StringValidation {
-        max_length: Some(5),
-        min_length: Some(5),
-        pattern: Some(r"^[A-Z]{2}[A-Z2-9]{3}$".into()),
-    }));
+    let regex = Regex::new(r"([^A-Z])([A-Z])").unwrap();
 
-    Schema::Object(s)
-}
+    let schema_name = regex
+        .replace_all(type_name, "$1-$2")
+        .into_owned()
+        .to_lowercase();
 
-fn coordinates_schema(gen: &mut SchemaGenerator) -> Schema {
-    let mut s = match String::json_schema(gen) {
-        Schema::Object(s) => s,
-        Schema::Bool(_) => panic!("Unexpected base schema"),
-    };
+    let schema = schema_for!(T);
 
-    s.metadata = Some(Box::new(schemars::schema::Metadata {
-        description: Some("The coordinates of the location".into()),
-        examples: vec![serde_json::Value::String("0917S 03220E".into())],
-        ..Default::default()
-    }));
+    let schema_json =
+        to_string_pretty(&schema).expect(&format!("Failed to serialize {type_name} schema"));
 
-    s.string = Some(Box::new(schemars::schema::StringValidation {
-        max_length: Some(12),
-        min_length: Some(12),
-        pattern: Some(r"^[0-9]{4}[NS] [0-9]{5}[WE]$".into()),
-    }));
+    let mut schema_file = File::create(format!("./schemas/{schema_name}.json"))?;
 
-    Schema::Object(s)
-}
+    schema_file.write_all(schema_json.as_bytes())?;
 
-fn main() {
-    //let schema = schema_for!(LoCodeExtension);
-    let settings = schemars::gen::SchemaSettings::draft2019_09().with(|s| {
-        s.option_nullable = true;
-        s.option_add_null_type = false;
-    });
-    let gen = settings.into_generator();
-    let mut schema = gen.into_root_schema_for::<LoCodeExtension>();
-    schema.schema.metadata = schema.schema.metadata.map(|m| Box::new({schemars::schema::Metadata {
-        id: Some("https://github.com/sine-fdn/locode-extension/blob/main/specs/locode_extension.json".into()),
-        ..(*m)
-    }}));
-    println!("{}", serde_json::to_string_pretty(&schema).unwrap());
+    println!("{schema_name}.json successfully created");
+
+    Ok(())
 }
