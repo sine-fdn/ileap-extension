9be70d1d24f33bc604a961b436f71dc2d8e9eec1
Raimundo Henriques <raimundo.henriques@gmail.com>
2024-07-04 16:42:45 +0100

    feat: generate random tce with toc or hoc as inputs

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -1,10 +1,44 @@
 use ileap_extension::*;
+use pact_data_model::WrappedDecimal;
 use quickcheck::{Arbitrary, Gen};
 use regex::Regex;
 use schemars::schema_for;
 use serde_json::to_string_pretty;
 use std::fs::File;
 use std::io::{Error, Write};
+
+pub(crate) fn update_arbitrary_tce(tce: &mut Tce, toc: Option<Toc>, hoc: Option<Hoc>) -> Tce {
+    tce.toc_id = match toc.clone() {
+        Some(toc) => Some(toc.toc_id),
+        None => None,
+    };
+
+    tce.hoc_id = match hoc.clone() {
+        Some(hoc) => Some(hoc.hoc_id),
+        None => None,
+    };
+
+    if (toc.is_none() && hoc.is_none()) && toc.is_some() && hoc.is_some() {
+        panic!("Either Toc or Hoc, but not both, must be provided");
+    }
+
+    match tce.toc_id {
+        Some(_) => {
+            let toc = toc.unwrap();
+            tce.co2e_wtw =
+                WrappedDecimal::from(toc.co2e_intensity_wtw.0 * tce.transport_activity.0);
+            tce.co2e_ttw =
+                WrappedDecimal::from(toc.co2e_intensity_ttw.0 * tce.transport_activity.0);
+        }
+        None => {
+            let hoc = hoc.unwrap();
+            tce.co2e_wtw = WrappedDecimal::from(hoc.co2e_intensity_wtw);
+            tce.co2e_ttw = WrappedDecimal::from(hoc.co2e_intensity_ttw);
+        }
+    }
+
+    tce.to_owned()
+}
 
 fn main() -> Result<(), Error> {
     // generate_schema::<ShipmentFootprint>()?;
@@ -14,9 +48,17 @@ fn main() -> Result<(), Error> {
 
     let mut og = Gen::new(10);
 
-    let tce = Tce::arbitrary(&mut og);
+    let mut tce_1 = Tce::arbitrary(&mut og);
+    let toc = Toc::arbitrary(&mut og);
 
-    println!("tce: {tce:?}");
+    let mut tce_2 = Tce::arbitrary(&mut og);
+    let hoc = Hoc::arbitrary(&mut og);
+
+    let tce_toc = update_arbitrary_tce(&mut tce_1, Some(toc), None);
+    let tce_hoc = update_arbitrary_tce(&mut tce_2, None, Some(hoc));
+
+    println!("tce_toc: {tce_toc:?}");
+    println!("tce_hoc: {tce_hoc:?}");
 
     Ok(())
 }
